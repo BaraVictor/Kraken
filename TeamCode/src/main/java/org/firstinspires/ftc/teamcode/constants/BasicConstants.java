66d79d94164c7f99321e31f4a4e77bbd79519749
BaraVictor <108233528+BaraVictor@users.noreply.github.com>
2024-11-18 20:33:27 +0200

    first commit (#1)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/BasicConstants.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+public class BasicConstants {
+    // Drive Train Constants
+    public static final double WHEEL_DIAMETER_INCHES = 4.0;
+    public static final double TICKS_PER_REVOLUTION = 537.6;
+    public static final double GEAR_RATIO = 1.0;
+
+    // Autonomous Constants
+    public static final double DRIVE_SPEED = 0.5;
+    public static final double TURN_SPEED = 0.4;
+
+    // Servo Positions
+    public static final double GRABBER_CLOSED = 0.6;
+    public static final double GRABBER_OPEN = 0.2;
+    public static final double WRIST_UP = 0.8;
+    public static final double WRIST_DOWN = 0.2;
+
+    // Safety Constants
+    public static final double MOTOR_POWER_LIMIT = 1.0;
+    public static final double SAFE_STOP_DELAY = 0.5;
+}
