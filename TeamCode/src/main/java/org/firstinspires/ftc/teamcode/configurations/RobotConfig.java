0d006c9a16f185c2e453d4821b5122f43430e6eb
BaraVictor <108233528+BaraVictor@users.noreply.github.com>
2024-11-19 17:40:32 +0200

    master (#2)
    * first commit
    
    * first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfig.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.configurations;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class RobotConfig {
+    // Motoare de deplasare
+    public static DcMotor frontLeftMotor;
+    public static DcMotor backLeftMotor;
+    public static DcMotor frontRightMotor;
+    public static DcMotor backRightMotor;
+
+    // Metodă pentru configurarea motoarelor
+    public RobotConfig(HardwareMap hardwareMap) {
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Setează direcțiile motoarelor
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setMotorPowers(double frontLeftPower, double backLeftPower, double frontRightPower, double backRightPower) {
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+}
