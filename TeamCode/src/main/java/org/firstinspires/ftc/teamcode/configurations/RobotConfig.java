fe09dac30095f27cf782a2c361c7c9d2ac8fa846
Bara Victor <alinapaulabara@gmail.com>
2024-12-02 22:00:47 +0200

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfig.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.configurations;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RobotConfig {
@@ -11,12 +12,24 @@ public class RobotConfig {
     public static DcMotor frontRightMotor;
     public static DcMotor backRightMotor;
 
-    // Metodă pentru configurarea motoarelor
+    // Servo outtake
+    public static Servo outtakeClawServo;
+    public static Servo outtakeWristRollServo;
+    public static Servo outtakeWristPitchServo;
+    public static Servo outtakeElbowRightServo;
+    public static Servo outtakeElbowLeftServo;
+
     public RobotConfig(HardwareMap hardwareMap) {
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        outtakeClawServo = hardwareMap.servo.get("outtakeClawServo");
+        outtakeWristRollServo = hardwareMap.servo.get("outtakeWristRollServo");
+        outtakeWristPitchServo = hardwareMap.servo.get("outtakeWristPitchServo");
+        outtakeElbowRightServo = hardwareMap.servo.get("outtakeElbowRightServo");
+        outtakeElbowLeftServo = hardwareMap.servo.get("outtakeElbowLeftServo");
 
         // Setează direcțiile motoarelor
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -30,5 +43,17 @@ public class RobotConfig {
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
+    }
+
+    public void setOuttakeServoPositions(double clawServoPosition,
+                                         double wristRollServoPosition,
+                                         double wristPitchServoPosition,
+                                         double elbowRightServoPosition,
+                                         double elbowLeftServoPosition) {
+        outtakeClawServo.setPosition(clawServoPosition);
+        outtakeWristRollServo.setPosition(wristRollServoPosition);
+        outtakeWristPitchServo.setPosition(wristPitchServoPosition);
+        outtakeElbowRightServo.setPosition(elbowRightServoPosition);
+        outtakeElbowLeftServo.setPosition(elbowLeftServoPosition);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/noncompetitional/teleOp/ServoAngleControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/noncompetitional/teleOp/ServoAngleControl.java
@@ -2,18 +2,17 @@ package org.firstinspires.ftc.teamcode.noncompetitional.teleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.configurations.RobotConfig;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Servo Angle Control", group = "Examples")
 public class ServoAngleControl extends LinearOpMode {
 
-    private Servo servo; // Definirea servo-ului
-
+    private RobotConfig robotConfig; // Definirea configurației robotului
 
     @Override
     public void runOpMode() {
         // Inițializarea hardware-ului
-        servo = hardwareMap.get(Servo.class, "servo");
-        double servoPosition = servo.getPosition();
+        double servoPosition = robotConfig.outtakeClawServo.getPosition();
         // Așteptăm să înceapă
         telemetry.addData("Status", "Waiting for start");
         telemetry.update();
@@ -24,18 +23,15 @@ public class ServoAngleControl extends LinearOpMode {
             double stickY = -gamepad1.left_stick_y; // Inversăm direcția pentru control mai intuitiv
 
             // Actualizarea poziției servo-ului în funcție de valoarea stick-ului
-            servoPosition += stickY * 0.01; // Incrementăm/decrementăm poziția servo-ului
+            servoPosition += stickY * 0.005; // Incrementăm/decrementăm poziția servo-ului
+
             servoPosition = Math.max(0.0, Math.min(1.0, servoPosition)); // Clamping între 0.0 și 1.0
 
             // Setăm poziția servo-ului
-            servo.setPosition(servoPosition);
-
-            // Calculăm unghiul servo-ului (presupunem că range-ul servo-ului este 0-180°)
-            double servoAngle = servoPosition * 180.0;
+            robotConfig.outtakeClawServo.setPosition(servoPosition);
 
             // Afișăm informații pe telemetrie
             telemetry.addData("Servo Position", servoPosition);
-            telemetry.addData("Servo Angle (degrees)", servoAngle);
             telemetry.update();
         }
     }
